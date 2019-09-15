0ff7ffa3d9cc7e9fd6c28caac3b586407e6f5038
Jay <psatdoctor@naver.com>
2019-09-15 21:11:59 +0900

    Add mongoose(mongodb)
    DB로케이션을 설정하고, 불러와두기

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 //express는 서버를 만들어준다. 상수는 압축 시켜놓은 것.
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const userRoutes = require("./Routes/user");
 const profileRoutes = require("./Routes/profile");
 const bbsRoutes = require('./Routes/bbs');
 
+const db = 'mongodb+srv://passme:passgosi1q2w@cluster0-ppakr.mongodb.net/test?retryWrites=true&w=majority';
+
+//mongoDB로 db의 주소를 불러온다.
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('mongodb connected..'))
+    .catch(err => console.log(err));
+    
 
 //use는 사용자가 /user라는 요청을 넣으면 앱(userRoutes)을 사용한다. 
 app.use('/user', userRoutes);
