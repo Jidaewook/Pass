5a3e17b495329c945b144844ea5ecc19316d6f06
Jay <psatdoctor@naver.com>
2019-09-15 20:51:41 +0900

    Add bbs/profile .post/patch/delete/get
    bbs와 profile의 등록/수정/삭제/불러오기

--- Routes/bbs.js
+++ Routes/bbs.js
@@ -2,4 +2,46 @@ const express = require('express');
 
 const router = express.Router();
 
+
+//@route localhost:3000/bbs
+//@desc bbs에 새로 등록(post)한 게시물을 데려온다. 
+//@auth private
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        msg: "bbs get"
+    })
+})
+
+//@route localhost:3000/bbs
+//@desc bbs에 새로 등록(post)한다.
+//@auth private
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg: 'bbs post'
+    })
+})
+
+
+//@route localhost:3000/bbs
+//@desc bbs에 등록(post)된 게시물을 수정(patch)한다.
+//@auth private
+
+router.patch('/', (req, res) => {
+    res.status(200).json({
+        msg: 'bbs patch'
+    })
+})
+
+//@route localhost:3000/bbs
+//@desc bbs에 등록(post)된 게시물을 삭제(delete)한다.
+//@auth private
+
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        msg: 'bbs delete'
+    })
+})
+
 module.exports = router;
--- Routes/profile.js
+++ Routes/profile.js
@@ -2,4 +2,45 @@ const express = require('express');
 
 const router = express.Router();
 
+//@route localhost:3000/profile
+//@desc profile에 새로 등록(post)한 게시물을 데려(get)온다. 
+//@auth private
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        msg: "profile get"
+    })
+})
+
+//@route localhost:3000/profile
+//@desc profile에 새로 등록(post)한다.
+//@auth private
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg: 'profile post'
+    })
+})
+
+
+//@route localhost:3000/profile
+//@desc profile에 등록(post)된 게시물을 수정(patch)한다.
+//@auth private
+
+router.patch('/', (req, res) => {
+    res.status(200).json({
+        msg: 'profile patch'
+    })
+})
+
+//@route localhost:3000/profile
+//@desc profile에 등록(post)된 게시물을 삭제(delete)한다.
+//@auth private
+
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        msg: 'profile delete'
+    })
+})
+
 module.exports = router;
